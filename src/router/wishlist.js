10ca6ba17278f7f2f94655375f0d044b6b6a7591
Akash Patil <akashpatil4659@gmail.com>
2025-02-12 18:55:23 +0530

    implement wishlist product delete route

--- src/router/wishlist.js
+++ src/router/wishlist.js
@@ -25,10 +25,43 @@ wishlistRouter.post("/wishlist/:productId", userAuth, async (req, res) => {
 
     if (!user) return res.status(404).json({ message: "User not found" });
 
+    const productExists = user.wishlist.find(
+      (id) => id.toString() === productId
+    );
+
+    if (productExists)
+      return res
+        .status(400)
+        .json({ message: "Product already exists in wishlist" });
+
     user.wishlist.push(productId);
     await user.save();
 
     res.json({ message: "Product added to wishlist successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+wishlistRouter.delete("/wishlist/:productId", userAuth, async (req, res) => {
+  try {
+    const { user } = req;
+    const { productId } = req.params;
+
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    const productExists = user.wishlist.find(
+      (id) => id.toString() === productId
+    );
+
+    if (!productExists)
+      return res.status(404).json({ message: "Product not found in wishlist" });
+
+    user.wishlist = user.wishlist.filter((id) => id.toString() !== productId);
+
+    await user.save();
+
+    res.json({ message: "Product removed from wishlist successfully" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
