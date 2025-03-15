1957101f2ea17c74f96002dde220d13725695393
Akash Patil <akashpatil4659@gmail.com>
2025-03-15 22:36:56 +0530

    modify code for server side rendering

--- src/app.js
+++ src/app.js
@@ -4,21 +4,33 @@ const cors = require("cors");
 const app = express();
 const connectDB = require("./config/database");
 const cookieParser = require("cookie-parser");
+const path = require("path");
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use(express.json());
 app.use(cookieParser());
 
+// Serve static files from the frontend build
+app.use(express.static(path.join(__dirname, "dist")));
+
 const cartRouter = require("./router/cart");
 const authRouter = require("./router/auth");
 const productsRouter = require("./router/products");
 const wishlistRouter = require("./router/wishlist");
 
-app.use("/", authRouter);
-app.use("/", cartRouter);
-app.use("/", productsRouter);
-app.use("/", wishlistRouter);
+app.use("/api", authRouter);
+app.use("/api", cartRouter);
+app.use("/api", productsRouter);
+app.use("/api", wishlistRouter);
+
+// Catch-all route to serve `index.html`
+app.get("*", (req, res) => {
+  res.setHeader("Cache-Control", "no-store");
+  res.sendFile(path.join(__dirname, "dist", "index.html"), {
+    cacheControl: false,
+  });
+});
 
 connectDB()
   .then(() => {
