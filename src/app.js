79f173026fd87a5daa3c9a058409220fadcaad7d
Akash Patil <akashpatil4659@gmail.com>
2025-03-15 21:45:49 +0530

    modify cors origin

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 const connectDB = require("./config/database");
 const cookieParser = require("cookie-parser");
 
-app.use(cors({ origin: "*", credentials: true }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use(express.json());
 app.use(cookieParser());
