15c080fdeca52228b7498ea1b41a185c84289bc2
Akash Patil <akashpatil4659@gmail.com>
2025-03-21 13:24:41 +0530

    add daisyUI component library

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+
 export default {
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  },
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyui],
 };
