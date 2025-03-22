9a2a1fc8bc8fc2cf3996f4d819e8bb340aa22f63
Akash Patil <akashpatil4659@gmail.com>
2025-03-22 23:28:15 +0530

    add updated build files to backend

---
+++ backend/src/dist/data/dropDownMenuData.js
@@ -0,0 +1,18 @@
+export const dropDownMenuData = [
+  {
+    name: "Account",
+    to: "/account",
+  },
+  {
+    name: "Wishlist",
+    to: "/wishlist",
+  },
+  {
+    name: "Cart",
+    to: "/cart",
+  },
+  {
+    name: "Settings",
+    to: "/settings",
+  },
+];
